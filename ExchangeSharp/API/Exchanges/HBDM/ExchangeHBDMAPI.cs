6dedb10b3d1ad7f9d58721a9133ecc8a85251f4e
henry <xbhuang1994@gmail.com>
2019-03-10 07:30:21 +0800

    添加火币合约交易所 完成火币合约的orderbook监听

---
+++ ExchangeSharp/API/Exchanges/HBDM/ExchangeHBDMAPI.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeSharp
+{
+    public sealed partial class ExchangeHBDMAPI : ExchangeAPI
+    {
+        public override string BaseUrl { get; set; } = "https://api.hbdm.com";
+        public string BaseUrlV1 { get; set; } = "https://api.hbdm.com/api/v1";
+        public override string BaseUrlWebSocket { get; set; } = "wss://www.hbdm.com/ws";
+        public string PrivateUrlV1 { get; set; } = "https://api.hbdm.com/api/v1";
+
+        public bool IsMargin { get; set; }
+        public string SubType { get; set; }
+
+        private long webSocketId = 0;
+
+        public ExchangeHBDMAPI()
+        {
+            RequestContentType = "application/x-www-form-urlencoded";
+            NonceStyle = NonceStyle.UnixMilliseconds;
+            MarketSymbolSeparator = string.Empty;
+            MarketSymbolIsUppercase = false;
+            WebSocketOrderBookType = WebSocketOrderBookType.FullBookAlways;
+        }
+
+        public override string ExchangeMarketSymbolToGlobalMarketSymbol(string marketSymbol)
+        {
+            if (marketSymbol.Length < 6)
+            {
+                throw new ArgumentException("Invalid market symbol " + marketSymbol);
+            }
+            else if (marketSymbol.Length == 6)
+            {
+                return ExchangeMarketSymbolToGlobalMarketSymbolWithSeparator(marketSymbol.Substring(0, 3) + GlobalMarketSymbolSeparator + marketSymbol.Substring(3, 3), GlobalMarketSymbolSeparator);
+            }
+            return ExchangeMarketSymbolToGlobalMarketSymbolWithSeparator(marketSymbol.Substring(3) + GlobalMarketSymbolSeparator + marketSymbol.Substring(0, 3), GlobalMarketSymbolSeparator);
+        }
+
+        public override string PeriodSecondsToString(int seconds)
+        {
+            return CryptoUtility.SecondsToPeriodStringLong(seconds);
+        }
+
+
+        #region Websocket API
+        protected override IWebSocket OnGetOrderBookWebSocket(Action<ExchangeOrderBook> callback, int maxCount = 20, params string[] marketSymbols)
+        {
+            return ConnectWebSocket(string.Empty, async (_socket, msg) =>
+            {
+                /*
+{{
+  "id": "id1",
+  "status": "ok",
+  "subbed": "market.btcusdt.depth.step0",
+  "ts": 1526749164133
+}}
+
+
+{{
+  "ch": "market.btcusdt.depth.step0",
+  "ts": 1526749254037,
+  "tick": {
+    "bids": [
+      [
+        8268.3,
+        0.101
+      ],
+      [
+        8268.29,
+        0.8248
+      ],
+      
+    ],
+    "asks": [
+      [
+        8275.07,
+        0.1961
+      ],
+	  
+      [
+        8337.1,
+        0.5803
+      ]
+    ],
+    "ts": 1526749254016,
+    "version": 7664175145
+  }
+}}
+                 */
+                var str = msg.ToStringFromUTF8Gzip();
+                JToken token = JToken.Parse(str);
+
+                if (token["status"] != null)
+                {
+                    return;
+                }
+                else if (token["ping"] != null)
+                {
+                    await _socket.SendMessageAsync(str.Replace("ping", "pong"));
+                    return;
+                }
+                var ch = token["ch"].ToStringInvariant();
+                var sArray = ch.Split('.');
+                var marketSymbol = sArray[1].ToStringInvariant();
+                ExchangeOrderBook book = ExchangeAPIExtensions.ParseOrderBookFromJTokenArrays(token["tick"], maxCount: maxCount);
+                book.MarketSymbol = marketSymbol;
+                callback(book);
+            }, async (_socket) =>
+            {
+                if (marketSymbols == null || marketSymbols.Length == 0)
+                {
+                    marketSymbols = (await GetMarketSymbolsAsync()).ToArray();
+                }
+                foreach (string symbol in marketSymbols)
+                {
+                    long id = System.Threading.Interlocked.Increment(ref webSocketId);
+                    //var normalizedSymbol = NormalizeMarketSymbol(symbol);
+                    string channel = $"market.{symbol}.depth.step0";
+                    await _socket.SendMessageAsync(new { sub = channel, id = "id" + id.ToStringInvariant() });
+                }
+            });
+        }
+        #endregion
+
+
+    }
+    public partial class ExchangeName { public const string HBDM = "HBDM"; }
+}
