dacc741c762dd927153ba41c9d7411edfe56151c
liuqiba <874740127@qq.com>
2019-03-11 18:58:50 +0800

    添加 火币 下单接口，修改 nlog.config 参数

---
+++ ExchangeSharp/ExchangeEnums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeSharp
+{
+    #region Enum
+    public enum Side
+    {
+        Buy = 0,
+        Sell,
+    }
+    //enum OrderType
+    //{
+    //    Limit = 0,
+    //    Market,
+    //}
+    public enum OrderResultType
+    {
+        Unknown = 0,
+        Filled,
+        FilledPartially,
+        Pending,
+        Error,
+        Canceled,
+        PendingCancel,
+    }
+    #endregion
+}
